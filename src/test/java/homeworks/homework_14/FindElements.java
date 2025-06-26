fd879124637592e75b470957ae6cd2f63fedb852
Eva Dieterle <eva.dieterle.esl@gmail.com>
2025-06-26 08:23:24 +0200

    Homework 14

--- src/test/java/homeworks/homework_14/FindElements.java
+++ src/test/java/homeworks/homework_14/FindElements.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 
 public class FindElements {
